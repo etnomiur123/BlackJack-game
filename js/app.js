83b1eca500c86fd4ab1ccdb2a52077eba00f54df
etnomiur123 <ruimonte_20@hotmail.com>
2020-11-26 20:06:26 +0000

    Added card svg's to the game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import cards from './cards.js';
+
 const elements = {
   score1: document.querySelector(".score-1"),
   score2: document.querySelector(".score-2"),
@@ -5,7 +7,12 @@ const elements = {
   button2: document.querySelector(".button-2"),
   next1: document.querySelector(".next-1"),
   next2: document.querySelector(".next-2"),
+  cardPlace: document.querySelector(".cardPlace")
 };
+
+const state = {
+  cards: [...cards],
+}
 
 const player1 = {
   score: 0,
@@ -52,9 +59,9 @@ const finish = (player) => {
 };
 
 const checkStop = () => {
-    if(player1.stop) changeTurn();
-    if(player2.stop) changeTurn();
-}
+  if (player1.stop) changeTurn();
+  if (player2.stop) changeTurn();
+};
 
 const checkWinner = () => {
   //check player1
@@ -96,20 +103,35 @@ const checkWinner = () => {
     }, 500);
     return;
   }
-
 };
+
+
+const getRandomCard = () => {
+  return state.cards[Math.floor(Math.random() * state.cards.length)];
+}
+
 
 //Player 1 controls
 
 elements.button1.addEventListener("click", () => {
   if (player1.active && player1.stop === false) {
-    const points = Math.floor(Math.random() * 10) + 1;
-    player1.score += points;
+    const card = getRandomCard()
+    if(card.value === "ace") {
+      if(21 - player1.score > 10) {
+        player1.score += 10
+      } else if(21 - player1.score < 10) {
+        player1.score += 1
+      }
+    } else {
+      player1.score += card.value;
+    }
+    elements.cardPlace.innerHTML = card.img;
+    console.log("player 1 => " + card.name)
     elements.score1.innerHTML = player1.score;
     checkWinner();
-    checkStop()
+    checkStop();
     changeTurn();
-  } 
+  }
 });
 
 elements.next1.addEventListener("click", () => {
@@ -121,11 +143,21 @@ elements.next1.addEventListener("click", () => {
 
 elements.button2.addEventListener("click", () => {
   if (player2.active && player2.stop === false) {
-    const points = Math.floor(Math.random() * 10) + 1;
-    player2.score += points;
+    const card = getRandomCard()
+    if(card.value === "ace") {
+      if(21 - player2.score > 10) {
+        player2.score += 10
+      } else if(21 - player2.score < 10) {
+        player2.score += 1
+      }
+    } else {
+      player2.score += card.value;
+    }
+    elements.cardPlace.innerHTML = card.img;
+    console.log("player 2 => " + card.name)
     elements.score2.innerHTML = player2.score;
     checkWinner();
-    checkStop()
+    checkStop();
     changeTurn();
   }
 });
