d2df17acee4b2bc8d9b00dcfbc1817e7bd5f48cc
etnomiur123 <ruimonte_20@hotmail.com>
2020-11-26 21:14:02 +0000

    Added UI Components

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-import cards from './cards.js';
+import cards from "./cards.js";
 
 const elements = {
+  playerName1: document.querySelector(".player1 h2"),
+  playerName2: document.querySelector(".player2 h2"),
   score1: document.querySelector(".score-1"),
   score2: document.querySelector(".score-2"),
   button1: document.querySelector(".button-1"),
   button2: document.querySelector(".button-2"),
   next1: document.querySelector(".next-1"),
   next2: document.querySelector(".next-2"),
-  cardPlace: document.querySelector(".cardPlace")
+  cardPlace: document.querySelector(".cardPlace"),
 };
 
 const state = {
   cards: [...cards],
-}
+};
 
 const player1 = {
   score: 0,
@@ -30,6 +32,8 @@ const changeTurn = () => {
   if (player1.active) {
     player1.active = false;
     player2.active = true;
+    elements.playerName1.classList.toggle("player-name-active")
+    elements.playerName2.classList.toggle("player-name-active")
     elements.button1.classList.remove("active");
     elements.next1.classList.remove("active");
     elements.button2.classList.add("active");
@@ -40,6 +44,8 @@ const changeTurn = () => {
   if (player2.active) {
     player2.active = false;
     player1.active = true;
+    elements.playerName1.classList.toggle("player-name-active")
+    elements.playerName2.classList.toggle("player-name-active")
     elements.button2.classList.remove("active");
     elements.next2.classList.remove("active");
     elements.button1.classList.add("active");
@@ -105,28 +111,26 @@ const checkWinner = () => {
   }
 };
 
-
 const getRandomCard = () => {
   return state.cards[Math.floor(Math.random() * state.cards.length)];
-}
-
+};
 
 //Player 1 controls
 
 elements.button1.addEventListener("click", () => {
   if (player1.active && player1.stop === false) {
-    const card = getRandomCard()
-    if(card.value === "ace") {
-      if(21 - player1.score > 10) {
-        player1.score += 10
-      } else if(21 - player1.score < 10) {
-        player1.score += 1
+    const card = getRandomCard();
+    if (card.value === "ace") {
+      if (21 - player1.score > 10) {
+        player1.score += 10;
+      } else if (21 - player1.score < 10) {
+        player1.score += 1;
       }
     } else {
       player1.score += card.value;
     }
     elements.cardPlace.innerHTML = card.img;
-    console.log("player 1 => " + card.name)
+    console.log("player 1 => " + card.name);
     elements.score1.innerHTML = player1.score;
     checkWinner();
     checkStop();
@@ -143,18 +147,18 @@ elements.next1.addEventListener("click", () => {
 
 elements.button2.addEventListener("click", () => {
   if (player2.active && player2.stop === false) {
-    const card = getRandomCard()
-    if(card.value === "ace") {
-      if(21 - player2.score > 10) {
-        player2.score += 10
-      } else if(21 - player2.score < 10) {
-        player2.score += 1
+    const card = getRandomCard();
+    if (card.value === "ace") {
+      if (21 - player2.score > 10) {
+        player2.score += 10;
+      } else if (21 - player2.score < 10) {
+        player2.score += 1;
       }
     } else {
       player2.score += card.value;
     }
     elements.cardPlace.innerHTML = card.img;
-    console.log("player 2 => " + card.name)
+    console.log("player 2 => " + card.name);
     elements.score2.innerHTML = player2.score;
     checkWinner();
     checkStop();
